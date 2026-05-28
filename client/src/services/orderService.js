273044f12707a036a52b969816bd74b523b895c7
nduong2810 <duongboil5@gmail.com>
2026-05-28 08:20:31 +0700

    Add order history and cancellation client APIs

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -1,3 +1,6 @@
 import apiClient from '../lib/apiClient';
 
 export const checkoutAPI = (data) => apiClient.post('/order/checkout', data);
+export const getMyOrdersAPI = () => apiClient.get('/order/my-orders');
+export const getMyOrderDetailAPI = (id) => apiClient.get(`/order/my-orders/${id}`);
+export const cancelMyOrderAPI = (id, reason) => apiClient.patch(`/order/my-orders/${id}/cancel`, { reason });
